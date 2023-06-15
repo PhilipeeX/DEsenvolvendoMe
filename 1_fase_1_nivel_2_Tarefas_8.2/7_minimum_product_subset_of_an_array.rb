cec7eaa67ba59d474cff5bc68698243502b5c5f6
Philipe <dev.philipe@outlook.com>
2023-06-14 21:46:26 -0300

    refactor: Função minimum_product refatorada

--- 1_fase_1_nivel_2_Tarefas_8.2/7_minimum_product_subset_of_an_array.rb
+++ 1_fase_1_nivel_2_Tarefas_8.2/7_minimum_product_subset_of_an_array.rb
@@ -2,17 +2,21 @@
 
 def minimum_product(array)
 
-  # If there are even number of negative numbers and no zeros, the result is the product of all except the largest valued negative number.
-  if array.count { |elemento| elemento < 0 } % 2 == 0 && array.none? { |elemento| elemento == 0}
+  # If there are even number of negative numbers and no zeros, the result is the product of all except the largest
+  # valued negative number.
+  if array.count(&:negative?).even? && array.none?(&:zero?)
     array.delete(array.min)
     return array.reduce { |result, element| result * element }
+
   # If there are an odd number of negative numbers and no zeros, the result is simply the product of all.
-  elsif array.count { |elemento| elemento < 0 } % 2 != 0 && array.none? { |elemento| elemento == 0}
+  elsif array.count(&:negative?).odd? && array.none?(&:zero?)
     return array.reduce { |result, element| result * element }
+
   # If there are zeros and positive, no negative, the result is 0.
-  elsif array.any? { |elemento| elemento == 0} && array.none? { |elemento| elemento < 0}
+  elsif array.any?(&:zero?) && array.none?(&:negative?)
     return 0
   end
+
   array.min
 end
 
