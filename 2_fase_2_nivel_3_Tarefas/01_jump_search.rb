bb65809d3d9a53443a9dd5a77f4c277999a7cad0
PhilipeeX <dev.philipe@outlook.com>
2023-06-18 13:08:41 -0300

    fix: jump_search method fixed all tests passed

--- 2_fase_2_nivel_3_Tarefas/01_jump_search.rb
+++ 2_fase_2_nivel_3_Tarefas/01_jump_search.rb
@@ -2,22 +2,24 @@ def jump_search(array, element)
   jumps = Math.sqrt(array.size).to_i
 
   i = 0
-  while array[[array.size, i].min - 1] < element && i < array.size
-    i += jumps
+  while array[[array.size, jumps].min - 1] < element
+    i = jumps
+    jumps += Math.sqrt(array.size).to_i
+    if i >= array.size
+      return nil
+    end
   end
 
-  if i >= array.size
-    i = array.size - 1
-  end
-  j = i
-  i -= jumps
-  while array[i] != array[j + 1]
-    return i if array[i] == element
-
+  while array[i] < element
     i += 1
+    if i == [array.size, jumps].min
+      return nil
+    end
   end
 
-  nil
+  if array[i] == element
+    return i
+  else
+    return nil
+  end
 end
-@array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
-puts jump_search(@array, 55)
