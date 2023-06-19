edf2056e930e30747e085404b03b6d9b9c633846
Philipe <dev.philipe@outlook.com>
2023-06-19 09:35:14 -0300

    refactor: finding_missing refactored

--- 2_fase_2_nivel_3_Tarefas/02_find_the_missing_number.rb
+++ 2_fase_2_nivel_3_Tarefas/02_find_the_missing_number.rb
@@ -1,13 +1,8 @@
 def finding_missing(arr, value)
   array = arr.sort
   i = 1
-  while i - 1 < value
+  (1..value).each do |i|
     return i if array[i - 1] != i
-
-    i += 1
   end
   'No missing value'
 end
-array = [1, 2, 3, 5]
-n = 5
-finding_missing(array, n)
