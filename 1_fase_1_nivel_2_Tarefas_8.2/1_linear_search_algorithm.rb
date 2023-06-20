a0de6c3a9b445a4b978c9f04c90f57518d80ada2
Philipe <dev.philipe@outlook.com>
2023-06-20 18:35:33 -0300

    Refactor: method find_element refactored

--- 1_fase_1_nivel_2_Tarefas_8.2/1_linear_search_algorithm.rb
+++ 1_fase_1_nivel_2_Tarefas_8.2/1_linear_search_algorithm.rb
@@ -1,14 +1,5 @@
-@array = Array.new(10) { rand(1..100) }
-
-def find_element(element)
-  i = 0
-  while i < @array.length
-    return "O elemento está presente no índice: #{i}" if @array[i] == element
-
-    i += 1
-  end
+def find_element(arr, element)
+  length = arr.length
+  (0..length).each { |i| return i if arr[i] == element }
   "O elemento escolhido #{element} não foi encontrado no array"
 end
-
-puts 'Escolha um valor de 1 à 100 para verificação'
-puts find_element(gets.chomp.to_i)
